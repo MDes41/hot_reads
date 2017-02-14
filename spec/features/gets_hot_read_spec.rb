0c7a65e50132eeac5fd10e9359721a19e5ab75c9
Matt D <MDes41@users.noreply.github.com>
2017-02-13 18:59:11 -0700

    Writing test to hit controller

---
+++ spec/features/gets_hot_read_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature 'Authenticated User' do
+  context 'goes to hot reads index' do
+    scenario 'hot reads are listed' do
+      link = "{\"id\":5,\"title\":\"asdf\",\"url\":\"http://www.example.com\",\"read\":false,\"created_at\":\"2017-02-14T01:49:09.941Z\",\"updated_at\":\"2017-02-14T01:49:09.941Z\",\"user_id\":null}"
+      conn = Faraday.new(:url => 'http://localhost:3000/urls')
+    # https://hot-reads.herokuapp.com
+
+      conn.post do |req|
+        req.body = link
+      end
+    end
+  end
+end
